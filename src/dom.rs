f2dda78c63d21159e768153f7ce6550f480d6e06
ochi <ocos54ce@gmail.com>
2024-04-24 20:31:56 +0900

    derive debug

--- src/dom.rs
+++ src/dom.rs
@@ -1,15 +1,15 @@
 use std::collections::HashMap;
-
+#[derive(Debug)]
 pub struct Node {
     children: Vec<Node>,
     node_type: NodeType,
 }
-
+#[derive(Debug)]
 enum NodeType {
     Text(String),
     Element(ElementData),
 }
-
+#[derive(Debug)]
 struct ElementData {
     tag_name: String,
     attributes: AttrMap,
